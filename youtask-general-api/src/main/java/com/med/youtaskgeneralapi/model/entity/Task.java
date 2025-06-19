948db08b312b2a8f992324c3581230531df24773
Mousta-Med <moustamuhammed@gmail.com>
2025-06-19 17:20:38 +0100

    create task entity with taskstatus enum

---
+++ youtask-general-api/src/main/java/com/med/youtaskgeneralapi/model/entity/Task.java
@@ -0,0 +1,39 @@
+package com.med.youtaskgeneralapi.model.entity;
+
+import com.med.youtaskgeneralapi.model.enums.TaskStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue()
+    private UUID id;
+
+    @NotBlank(message = "Title is required")
+    @Size(max = 100, message = "Title must not exceed 100 characters")
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Size(max = 500, message = "Description must not exceed 500 characters")
+    @Column(length = 500)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TaskStatus status = TaskStatus.PENDING;
+
+}
---
+++ youtask-general-api/src/main/java/com/med/youtaskgeneralapi/model/enums/TaskStatus.java
@@ -0,0 +1,7 @@
+package com.med.youtaskgeneralapi.model.enums;
+
+public enum TaskStatus {
+    PENDING,
+    IN_PROGRESS,
+    COMPLETED
+}
