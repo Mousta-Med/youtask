f2e0792975aeab04a57c8bd5740ed450fc2db743
Mousta-Med <moustamuhammed@gmail.com>
2025-06-19 11:38:14 +0100

    initilize backend spring boot project

---
+++ youtask-general-api/src/main/java/com/med/youtaskgeneralapi/YoutaskGeneralApiApplication.java
@@ -0,0 +1,13 @@
+package com.med.youtaskgeneralapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutaskGeneralApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoutaskGeneralApiApplication.class, args);
+    }
+
+}
---
+++ youtask-general-api/src/test/java/com/med/youtaskgeneralapi/YoutaskGeneralApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.med.youtaskgeneralapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutaskGeneralApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
