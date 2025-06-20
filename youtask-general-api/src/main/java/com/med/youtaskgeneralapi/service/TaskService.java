364c75ac6317672d476c5986814b232352967262
Mousta-Med <moustamuhammed@gmail.com>
2025-06-20 11:17:39 +0100

    update Task service to use DTOs instead of accessing Entity directly

--- youtask-general-api/src/main/java/com/med/youtaskgeneralapi/service/TaskService.java
+++ youtask-general-api/src/main/java/com/med/youtaskgeneralapi/service/TaskService.java
@@ -1,8 +1,10 @@
 package com.med.youtaskgeneralapi.service;
 
+import com.med.youtaskgeneralapi.model.dto.TaskRequest;
+import com.med.youtaskgeneralapi.model.dto.TaskResponse;
 import com.med.youtaskgeneralapi.model.entity.Task;
 
 import java.util.UUID;
 
-public interface TaskService extends BaseService<Task, UUID, Task>{
+public interface TaskService extends BaseService<TaskRequest, UUID, TaskResponse>{
 }
--- youtask-general-api/src/main/java/com/med/youtaskgeneralapi/service/impl/TaskServiceImpl.java
+++ youtask-general-api/src/main/java/com/med/youtaskgeneralapi/service/impl/TaskServiceImpl.java
@@ -1,8 +1,11 @@
 package com.med.youtaskgeneralapi.service.impl;
 
+import com.med.youtaskgeneralapi.model.dto.TaskRequest;
+import com.med.youtaskgeneralapi.model.dto.TaskResponse;
 import com.med.youtaskgeneralapi.model.entity.Task;
 import com.med.youtaskgeneralapi.repository.TaskRepository;
 import com.med.youtaskgeneralapi.service.TaskService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,37 +13,46 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class TaskServiceImpl implements TaskService {
 
     private final TaskRepository taskRepository;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public TaskServiceImpl(TaskRepository taskRepository) {
+    public TaskServiceImpl(TaskRepository taskRepository,  ModelMapper modelMapper) {
         this.taskRepository =  taskRepository;
+        this.modelMapper = modelMapper;
     }
 
     @Override
-    public Task save(Task task) {
-        return taskRepository.save(task);
+    public TaskResponse save(TaskRequest taskrequest) {
+        Task task = modelMapper.map(taskrequest, Task.class);
+        return modelMapper.map(taskRepository.save(task), TaskResponse.class);
     }
 
     @Override
-    public List<Task> findAll() {
-        return taskRepository.findAll();
+    public List<TaskResponse> findAll() {
+        return taskRepository
+                .findAll()
+                .stream()
+                .map(task -> modelMapper.map(task, TaskResponse.class))
+                .collect(Collectors.toList());
     }
 
     @Override
-    public Task findOne(UUID uuid) {
-        return taskRepository.findById(uuid).orElseThrow(() -> new NoSuchElementException("Task Not found with this: " + uuid));
+    public TaskResponse findOne(UUID uuid) {
+        return taskRepository.findById(uuid)
+                .map(task -> modelMapper.map(task, TaskResponse.class)).orElseThrow(() -> new NoSuchElementException("Task Not found with this: " + uuid));
     }
 
     @Override
-    public Task update(UUID uuid, Task task) {
+    public TaskResponse update(UUID uuid, TaskRequest task) {
         Task existingtask =  taskRepository.findById(uuid).orElseThrow(() -> new NoSuchElementException("Task Not found with this: " + uuid));
         BeanUtils.copyProperties(task, existingtask);
-        return taskRepository.save(existingtask);
+        return modelMapper.map(taskRepository.save(existingtask), TaskResponse.class);
     }
 
     @Override
