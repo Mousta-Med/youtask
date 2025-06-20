90a4fc5795200ebd085e32c9c3acec438f2a911a
Mousta-Med <moustamuhammed@gmail.com>
2025-06-20 10:11:28 +0100

    Add global exception handler and use proper exception for entity not found in TaskService

--- youtask-general-api/src/main/java/com/med/youtaskgeneralapi/service/impl/TaskServiceImpl.java
+++ youtask-general-api/src/main/java/com/med/youtaskgeneralapi/service/impl/TaskServiceImpl.java
@@ -3,12 +3,12 @@ package com.med.youtaskgeneralapi.service.impl;
 import com.med.youtaskgeneralapi.model.entity.Task;
 import com.med.youtaskgeneralapi.repository.TaskRepository;
 import com.med.youtaskgeneralapi.service.TaskService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.UUID;
 
 @Service
@@ -33,19 +33,19 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public Task findOne(UUID uuid) {
-        return taskRepository.findById(uuid).orElseThrow(() -> new NoSuchElementException("Task Not found with this: " + uuid));
+        return taskRepository.findById(uuid).orElseThrow(() -> new EntityNotFoundException("Task Not found with this: " + uuid));
     }
 
     @Override
     public Task update(UUID uuid, Task task) {
-        Task existingtask =  taskRepository.findById(uuid).orElseThrow(() -> new NoSuchElementException("Task Not found with this: " + uuid));
+        Task existingtask =  taskRepository.findById(uuid).orElseThrow(() -> new EntityNotFoundException("Task Not found with this: " + uuid));
         BeanUtils.copyProperties(task, existingtask);
         return taskRepository.save(existingtask);
     }
 
     @Override
     public void delete(UUID uuid) {
-        taskRepository.findById(uuid).orElseThrow(() -> new NoSuchElementException("Task Not found with this: " + uuid));
+        taskRepository.findById(uuid).orElseThrow(() -> new EntityNotFoundException("Task Not found with this: " + uuid));
         taskRepository.deleteById(uuid);
     }
 }
