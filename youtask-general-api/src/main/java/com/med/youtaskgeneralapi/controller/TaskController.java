f58553a4fdfa27a6328d0a45d68fe0ba443c2fdb
Mousta-Med <moustamuhammed@gmail.com>
2025-06-20 11:24:09 +0100

    update Task controller to work with DTOs

--- youtask-general-api/src/main/java/com/med/youtaskgeneralapi/controller/TaskController.java
+++ youtask-general-api/src/main/java/com/med/youtaskgeneralapi/controller/TaskController.java
@@ -1,5 +1,7 @@
 package com.med.youtaskgeneralapi.controller;
 
+import com.med.youtaskgeneralapi.model.dto.TaskRequest;
+import com.med.youtaskgeneralapi.model.dto.TaskResponse;
 import com.med.youtaskgeneralapi.model.entity.Task;
 import com.med.youtaskgeneralapi.service.TaskService;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +11,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/task")
-public class TaskController extends BaseController<Task, UUID, Task, TaskService>{
+public class TaskController extends BaseController<TaskRequest, UUID, TaskResponse, TaskService>{
     protected TaskController(TaskService service) {
         super(service);
     }
