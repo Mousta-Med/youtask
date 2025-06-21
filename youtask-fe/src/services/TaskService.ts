3d9d479cf314cfb0d9df93c070bd7c45e36dc174
Mousta-Med <moustamuhammed@gmail.com>
2025-06-21 11:36:50 +0100

    add http-common & TaskService for task management API integration

---
+++ youtask-fe/src/services/TaskService.ts
@@ -0,0 +1,32 @@
+import http from "../http-common";
+import type Task from "../models/Task.model";
+
+const getAll = () => {
+  return http.get<Array<Task>>("/tasks");
+};
+
+const get = (id: string) => {
+  return http.get<Task>(`/task/${id}`);
+};
+
+const create = (data: Task) => {
+  return http.post<Task>("/task", data);
+};
+
+const update = (id: string, data: Task) => {
+  return http.put<Task>(`/task/${id}`, data);
+};
+
+const remove = (id: string) => {
+  return http.delete<void>(`/task/${id}`);
+};
+
+const TaskService = {
+  getAll,
+  get,
+  create,
+  update,
+  remove,
+};
+
+export default TaskService;
