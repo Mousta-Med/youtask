c995c1b6200c110c137df23f8ffb6dab8553d56e
Mousta-Med <moustamuhammed@gmail.com>
2025-06-23 12:48:35 +0100

    add CrossOrigin annotation in BaseController

--- youtask-general-api/src/main/java/com/med/youtaskgeneralapi/controller/BaseController.java
+++ youtask-general-api/src/main/java/com/med/youtaskgeneralapi/controller/BaseController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:5173,"})
 public abstract class BaseController<Request, Id, Response, Service extends BaseService<Request, Id, Response>> {
 
     private final Service service;
