34e6f68586599b4288836b87853c7fbe85db4b5e
Mousta-Med <moustamuhammed@gmail.com>
2025-06-25 17:43:27 +0100

    Add SecurityConfig class with JWT authentication and authorization setup

---
+++ youtask-general-api/src/main/java/com/med/youtaskgeneralapi/config/SecurityConfig.java
@@ -0,0 +1,72 @@
+package com.med.youtaskgeneralapi.config;
+
+import com.med.youtaskgeneralapi.jwt.JWTAuthenticationFilter;
+import com.med.youtaskgeneralapi.service.impl.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JWTAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        req -> req
+                                .requestMatchers(getOpenedResources()).permitAll()
+                                .anyRequest().authenticated()
+                )
+                .userDetailsService(userDetailsService)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .build();
+    }
+
+    private String[] getOpenedResources() {
+        return new String[]{
+                "/h2-console/**",
+                "/api/users/generate",
+                "/api/users/batch",
+                "/api/auth",
+                "/swagger-ui/**",
+                "/swagger-resources",
+                "/swagger-resources/**",
+                "/v3/api-docs/**"
+        };
+    }
+}
