12ba6f6ccd743b09361451ce392680adb2b3f179
Mousta-Med <moustamuhammed@gmail.com>
2025-06-25 14:57:18 +0100

    Implement auditing with Spring Security by adding AuditorAware implementation

---
+++ youtask-general-api/src/main/java/com/med/youtaskgeneralapi/audit/SpringSecurityAuditAwareImpl.java
@@ -0,0 +1,21 @@
+package com.med.youtaskgeneralapi.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.lang.NonNull;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class SpringSecurityAuditAwareImpl implements AuditorAware<String> {
+
+    @Override
+    @NonNull
+    public Optional<String> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!authentication.isAuthenticated()) {
+            return Optional.of("");
+        }
+        return Optional.of(authentication.getName());
+    }
+}
